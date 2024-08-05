fc544ef597b5a38b577301f8d16d8c55c8404973
David Fecke <david@wunderbit.de>
2024-08-05 22:58:53 +0200

    add option to separate config file from container app

--- gateway/Config.ts
+++ gateway/Config.ts
@@ -1,9 +1,21 @@
 import { parse } from "yaml"
 import { resolve } from "path"
-import { readFile } from "fs/promises"
+import { constants, access, readFile } from "fs/promises"
 import { Configuration } from "./types"
 
-const defaultConfigFile = resolve(__dirname, "..", "config.yaml")
-export default async function readConfig(configFile = defaultConfigFile) {
+const defaultPath = resolve( "/etc", "mqtt-prometheus-gateway", "config.yaml");
+const fallbackPath = resolve(__dirname, "..", "config.yaml");
+
+export default async function readConfig(configFile?: string) {
+
+  if (!configFile) {
+    try {
+      await access(defaultPath, constants.R_OK);
+      configFile = defaultPath;
+    } catch {
+      configFile = fallbackPath;
+    }
+  }
+
   return parse((await readFile(configFile)).toString()) as Configuration
 }
